6289aec6e609b5f3651c2be78d0f1d0f04448dbf
n.yoshioka <n.yoshioka@grow-e.com>
2020-09-04 18:47:55 +0900

    権限による画面アクセス許可設定の仕組みを追加

--- demoLogin.1.0/app/Providers/AuthServiceProvider.php
+++ demoLogin.1.0/app/Providers/AuthServiceProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -7,24 +6,44 @@ use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
-    ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		'App\Model' => 'App\Policies\ModelPolicy'
+	];
 
-        //
-    }
+	/**
+	 * Register any authentication / authorization services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerPolicies();
+
+		/**
+		 * 開発者ロール
+		 */
+		Gate::define('developer', function ($user) {
+			return ($user->role == 1);
+		});
+
+		/**
+		 * システム管理者ロール
+		 */
+		Gate::define('admin', function ($user) {
+			return ($user->role > 0 && $user->role <= 2);
+		});
+
+		/**
+		 * 一般ユーザロール
+		 */
+		Gate::define('user', function ($user) {
+			return ($user->role > 0 && $user->role <= 10);
+		});
+	}
 }
--- demoLogin.1.0/routes/web.php
+++ demoLogin.1.0/routes/web.php
@@ -1,23 +1,60 @@
 <?php
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | Web Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register web routes for your application. These
+ * | routes are loaded by the RouteServiceProvider within a group which
+ * | contains the "web" middleware group. Now create something great!
+ * |
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
-Auth::routes();
+/**
+ * ユーザー登録リンク非表示
+ */
+Auth::routes([
+	'register' => false
+]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+/**
+ * ユーザロール
+ */
+Route::group([
+	'middleware' => [
+		'auth',
+		'can:user'
+	]
+], function () {
+	Route::get('/home', 'HomeController@index')->name('home');
+});
+
+/**
+ * 管理者ロール
+ */
+Route::group([
+	'middleware' => [
+		'auth',
+		'can:admin'
+	]
+], function () {});
+
+/**
+ * 開発者ロール
+ * 緊急メンテや運用で使用するコントローラ等を設定する。
+ */
+Route::group([
+	'middleware' => [
+		'auth',
+		'can:developer'
+	]
+], function () {});
+
+

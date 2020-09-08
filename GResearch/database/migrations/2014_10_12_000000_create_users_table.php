806ccbfbce3689120e2803b1b68dc647172cd53e
n.yoshioka <n.yoshioka@grow-e.com>
2020-09-08 09:49:26 +0900

    ユーザーテーブル定義に権限管理のためのカラム（role）を追加

--- GResearch/database/migrations/2014_10_12_000000_create_users_table.php
+++ GResearch/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        	$table->id();
+        	$table->string('name');
+        	$table->string('email')->unique();
+        	$table->timestamp('email_verified_at')->nullable();
+        	$table->string('password');
+        	$table->smallInteger('role')
+        	->nullable(false)
+        	->default(10)
+        	->index();
+        	$table->rememberToken();
+        	$table->timestamps();
         });
     }
 
